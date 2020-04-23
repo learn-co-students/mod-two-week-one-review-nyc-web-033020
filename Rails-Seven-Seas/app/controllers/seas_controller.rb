1489c0bf03018cd202f73b340032da93ad529c24
dkersten <dkersten1994@gmail.com>
2020-04-23 14:58:51 -0400

    got  index, new, create, show actions working

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -5,6 +5,36 @@ class SeasController < ApplicationController
     @seas = Sea.all
   end
 
+  def new
+    # @sea = Sea.new(sea_params)
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+
+    # redirect_to 'show.html'
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+
+#   /photos/:id	GET	show
+# /photos/:id/edit	GET	edit
+# /photos/:id	PATCH/PUT	update
+# /photos/:id	DELETE	destroy
+
   private
   # In controller actions, use this private method to access sea params from forms.
   # Example: @sea.update(sea_params)
