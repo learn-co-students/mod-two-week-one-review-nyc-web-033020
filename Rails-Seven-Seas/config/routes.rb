9911eef4a514bf99fb69c090e0d83adae6a52e33
jnzhang622 <jnzhang622@gmail.com>
2020-04-23 15:37:36 -0400

    m

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/seas', to: 'seas#index'
-  get '/seas/new', to: 'seas#new'
-  # post '/seas', to: 'seas#show'
-  # get '/seas/:id',
-  # get 'seas/:id/edit'
-  # patch '/articles/:id'
-  # put '/articles/:id'
-  # delete '/articles/:id'
+  
+   get '/seas', to: 'seas#index'
+   get '/seas/new', to: 'seas#new'
+   get '/seas/:id/edit', to: "seas#edit"
+   get '/seas/:id', to: 'seas#show'
+   post '/seas', to: "seas#create"
+   put '/seas/:id', to: "seas#update"
+   delete '/seas/:id', to: 'seas#destroy'
+   
 end
