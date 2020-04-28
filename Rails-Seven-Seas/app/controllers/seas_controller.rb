6e2e72cb9355c11128f0a0e4086620008e2ae430
github username <github email address>
2020-04-27 21:47:44 -0400

    commit

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -23,11 +23,12 @@ class SeasController < ApplicationController
   def update(args)
     @sea = Sea.find(params[:id]) 
     @sea.update(sea_params)   
+    redirect to seas_path(@sea)
   end 
   def delete 
     @sea = Sea.find(params[:id]) 
     @sea.destroy 
-    redirect to "/seas" 
+    redirect to seas_path
   end 
   private
   def sea_params
